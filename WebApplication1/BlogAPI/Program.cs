ce110340b2c0939798a7cca773a1bdcede71da5f
muja-b <s11819212@stu.najah.edu>
2023-01-07 05:40:48 +0200

    managed the dependancy injection for the services

--- WebApplication1/BlogAPI/Program.cs
+++ WebApplication1/BlogAPI/Program.cs
@@ -1,4 +1,5 @@
 using WebApplication1.Middlewares;
+using WebApplication1.services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,9 @@ builder.Services.AddControllers();
 builder.Services.AddTransient <Authentication> ();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<modelContext>();
+builder.Services.AddScoped<IPostsService,PostsService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();

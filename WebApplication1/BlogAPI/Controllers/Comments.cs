08c23961e98ce8e7b7d069ea2db84fb65d241da3
muja-b <s11819212@stu.najah.edu>
2023-01-07 05:37:09 +0200

    added controllers functionallity

--- WebApplication1/BlogAPI/Controllers/Comments.cs
+++ WebApplication1/BlogAPI/Controllers/Comments.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.services;
 
 namespace WebApplication1.Controllers;
 
@@ -6,9 +7,20 @@ namespace WebApplication1.Controllers;
 [Route("[controller]")]
 public class Comments: ControllerBase
 {
-    [HttpGet(Name = "Comments")]
-    public Task<ActionResult<PostDTO>> Get()
+    private ICommentService _commentService;
+
+    public Comments(ICommentService commentService)
     {
-        return Ok();
+        _commentService = commentService;
+    }
+    [HttpGet(Name = "Comments")]
+    public async Task<ActionResult<PostDTO>> Get(int PostId)
+    {
+        return Ok(await _commentService.PostComments(PostId));
+    }
+    [HttpPost(Name = "Comments")]
+    public async Task<ActionResult<PostDTO>> Post(Comment comment)
+    {
+        return Ok(await _commentService.AddComment(comment));
     }
 }
--- WebApplication1/BlogAPI/Controllers/Posts.cs
+++ WebApplication1/BlogAPI/Controllers/Posts.cs
@@ -1,15 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.services;
 
 namespace WebApplication1.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class PostController : ControllerBase
 {
-    
-    [HttpGet(Name = "Posts")]
-    public Task<ActionResult<PostDTO>> Get()
+    private IPostsService _postsService;
+
+    public PostController(IPostsService postsService)
     {
-        return Ok();
+        _postsService = postsService;
+    }
+    [HttpGet(Name = "Posts")]
+    public async Task<ActionResult<List<Post>>> Get()
+    {
+        return  Ok(await _postsService.getPosts());
+    }
+
+    [HttpPost(Name = "Post")]
+    public async Task<ActionResult<bool>> Post(Post post)
+    {
+        return Ok(await _postsService.addPosts(post));
+    }
+
+    [HttpPost("Like")]
+    public async Task<ActionResult<bool>> LikePost(int postId)
+    {
+        return Ok(await _postsService.likePost(postId));
     }
 }
